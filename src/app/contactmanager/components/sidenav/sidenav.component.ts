372624424e62d3cb1e11fa331289906e6ebe2b5a
Robin Bette <robinbette@hotmail.com>
2021-12-01 16:02:18 +0100

    * Use breakpoint observer for small screens * Show icon only when small screen * Add cart for main content

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,15 +1,26 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  isScreenSmall!: boolean;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
+    // observe changes when the viewport resizes
+    this.breakpointObserver
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        // set isScreenSmall to if the state matches,
+        // as we observe if the small custom defined breakpoint has been hit
+        this.isScreenSmall = state.matches;
+      });
   }
-
 }
