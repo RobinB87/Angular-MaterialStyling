02c7624bd082b6c099cb74df6630642559a0c6ed
Robin Bette <robinbette@hotmail.com>
2021-12-01 14:14:01 +0100

    * Install flex-layout module

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { DemoRoutingModule } from './demo-routing.module';
 import { MaterialModule } from '../shared/material.module';
+import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
   declarations: [ButtonsComponent, FlexboxComponent],
-  imports: [CommonModule, DemoRoutingModule, MaterialModule, FormsModule],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+  ],
 })
 export class DemoModule {}
